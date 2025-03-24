d4711825656e58d149f49d45c2e3aa89dc251854
nilmar <nilmar@gmail.com>
2025-03-24 20:24:25 -0300

    Limpar o campo de texto ao usuário teclar Esc e Criar um botão limpar que apague a mensagem e o campo de texto ao lado do botão

--- hello-world.js
+++ hello-world.js
@@ -1,9 +1,20 @@
 const nameInput = document.getElementById('name');
 const messageP = document.getElementById('message');
 
+function clearMessage() {
+    nameInput.value = ''
+    messageP.innerText = ''
+}
+
 nameInput.onkeyup = (event) => {
     if (nameInput.value === '') {
         messageP.innerText = '';
+    }
+}
+
+nameInput.onkeydown = (event) => {
+    if(event.key === 'Escape'){
+        clearMessage();
     }
 }
 
